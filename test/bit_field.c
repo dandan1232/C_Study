a655b5bbee794c34c5f72d41090ff3beb2c51608
dandan1232 <345652127@qq.com>
2024-02-15 15:19:04 +0800

    C位域

---
+++ test/bit_field.c
@@ -0,0 +1,31 @@
+/*
+ @author: lindand  
+ @date: 2024-02-15
+ @description:C位域
+*/
+
+
+#include <stdio.h>
+#include <string.h>
+
+/* 定义简单的结构 */
+struct
+{
+    unsigned int widthValidated;
+    unsigned int heightValidated;
+} status1;
+
+/* 定义位域结构 */
+struct
+{
+    unsigned int widthValidated : 1;
+    unsigned int heightValidated : 1;
+} status2;
+
+int main( )
+{
+    printf( "Memory size occupied by status1 : %d\n", sizeof(status1));
+    printf( "Memory size occupied by status2 : %d\n", sizeof(status2));
+
+    return 0;
+}
