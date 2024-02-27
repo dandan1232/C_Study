5c8827b641bad56a463ab14f7b14ad7444c06d5f
dandan1232 <345652127@qq.com>
2024-02-27 15:54:24 +0800

    希尔排序

---
+++ sort/ShellSort.c
@@ -0,0 +1,21 @@
+/*
+ @author: lindand  
+ @date: 2024-02-27
+ @description:
+希尔排序，也称递减增量排序算法，是插入排序的一种更高效的改进版本。希尔排序是非稳定排序算法。
+希尔排序是基于插入排序的以下两点性质而提出改进方法的：
+插入排序在对几乎已经排好序的数据操作时，效率高，即可以达到线性排序的效率
+但插入排序一般来说是低效的，因为插入排序每次只能将数据移动一位
+*/
+
+void shell_sort(int arr[], int len) {
+    int gap, i, j;
+    int temp;
+    for (gap = len >> 1; gap > 0; gap = gap >> 1)
+        for (i = gap; i < len; i++) {
+            temp = arr[i];
+            for (j = i - gap; j >= 0 && arr[j] > temp; j -= gap)
+                arr[j + gap] = arr[j];
+            arr[j + gap] = temp;
+        }
+}
